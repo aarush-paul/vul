661b2b9a5b1c2ec079f4a4ca51aae7a7e0da8b0f
Aarush Paul <paulaarush97@gmail.com>
2023-07-21 21:12:16 +0530

    fredfgretg

--- script.js
+++ script.js
@@ -1,91 +1,83 @@
 // script.js
+let apiKey; // Variable to store the API key
+
 document.getElementById('appForm').addEventListener('submit', function (event) {
-	event.preventDefault();
-	const appName = document.getElementById('appName').value;
-	fetchAppReport(appName);
+  event.preventDefault();
+  const appName = document.getElementById('appName').value;
+  fetchAppReport(appName);
+});
+
+// Function to fetch the API key (Assuming you have a way to generate/get the API key)
+function getAPIKey() {
+  // Replace this with your logic to fetch or generate the API key
+  // For simplicity, let's assume there's a function that returns the key
+  return "YOUR_API_KEY";
+}
+
+// Function to refresh the API key every hour
+function refreshAPIKey() {
+  apiKey = getAPIKey();
+}
+
+// Fetch the initial API key and start the key refresh process
+refreshAPIKey();
+setInterval(refreshAPIKey, 60 * 60 * 1000); // Refresh the API key every hour
+
+function fetchAppReport(appName) {
+  const reportDiv = document.getElementById('report');
+  reportDiv.innerHTML = 'Loading...';
+
+  // Fetch actual data from the DownDetector API using the current API key
+  fetch(`https://api.downdetector.com/v1/reports/${appName}?apikey=${apiKey}`)
+    .then(response => response.json())
+    .then(reportData => {
+      displayReport(reportData);
+      displayMostReportedProblems(reportData.mostReportedProblems);
+      displayOutageGraph(reportData.outageGraphData);
+    })
+    .catch(error => {
+      console.error('Error fetching app report:', error);
+      reportDiv.innerHTML = 'Error fetching app report.';
+    });
+}
+
+function displayReport(reportData) {
+  const reportDiv = document.getElementById('report');
+  reportDiv.innerHTML = `
+    <h2>${reportData.name} Report</h2>
+    <p>Status: ${reportData.status}</p>
+    <p>Outages: ${reportData.outages}</p>
+    <p>Downvotes: ${reportData.downvotes}</p>
+    <p>Upvotes: ${reportData.upvotes}</p>
+  `;
+
+  reportDiv.style.display = 'block';
+}
+
+function displayMostReportedProblems(mostReportedProblems) {
+  const problemsDiv = document.getElementById('problems');
+  problemsDiv.innerHTML = '<h2>Most Reported Problems</h2>';
+
+  mostReportedProblems.forEach(problem => {
+    const p = document.createElement('p');
+    p.textContent = `${problem.name}: ${problem.count} reports`;
+    problemsDiv.appendChild(p);
   });
-  
-  function fetchAppReport(appName) {
-	const reportDiv = document.getElementById('report');
-	reportDiv.innerHTML = 'Loading...';
-  
-	// Fetch actual data from the DownDetector API or other sources
-	// In this example, we'll use sample data
-	const reportData = getSampleReportData(appName);
-  
-	displayReport(reportData);
-	displayMostReportedProblems(reportData.mostReportedProblems);
-	displayOutageGraph(reportData.outageGraphData);
-  }
-  
-  function getSampleReportData(appName) {
-	// Sample data for the report
-	return {
-	  appName,
-	  status: 'Operational',
-	  issues: 'No known issues',
-	  outage: 'No recent outage reported',
-	  lastUpdated: new Date().toLocaleString(),
-	  mostReportedProblems: [
-		{ problem: 'Login issues', count: 56 },
-		{ problem: 'Streaming problems', count: 34 },
-		{ problem: 'App crashes', count: 23 },
-		{ problem: 'Playback error', count: 19 },
-		{ problem: 'Search not working', count: 15 },
-	  ],
-	  outageGraphData: {
-		labels: ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM'],
-		datasets: [
-		  {
-			label: 'Spotify Outage',
-			data: [0, 0, 0, 0, 1, 0, 0],
-			borderColor: 'rgba(75, 192, 192, 1)',
-			backgroundColor: 'rgba(75, 192, 192, 0.2)',
-			borderWidth: 2,
-		  },
-		],
-	  },
-	};
-  }
-  
-  function displayReport(reportData) {
-	const reportDiv = document.getElementById('report');
-	reportDiv.innerHTML = `
-	  <h2>${reportData.appName} Report</h2>
-	  <p>Status: ${reportData.status}</p>
-	  <p>Issues: ${reportData.issues}</p>
-	  <p>Outage: ${reportData.outage}</p>
-	  <p>Last Updated: ${reportData.lastUpdated}</p>
-	`;
-  
-	reportDiv.style.display = 'block';
-  }
-  
-  function displayMostReportedProblems(mostReportedProblems) {
-	const problemsDiv = document.getElementById('problems');
-	problemsDiv.innerHTML = '<h2>Most Reported Problems</h2>';
-  
-	mostReportedProblems.forEach(problem => {
-	  const p = document.createElement('p');
-	  p.textContent = `${problem.problem}: ${problem.count} reports`;
-	  problemsDiv.appendChild(p);
-	});
-  }
-  
-  function displayOutageGraph(graphData) {
-	const graphCanvas = document.getElementById('outageGraph');
-	graphCanvas.innerHTML = '<h2>Spotify Outage Graph</h2>';
-  
-	new Chart(graphCanvas, {
-	  type: 'line',
-	  data: graphData,
-	  options: {
-		scales: {
-		  y: {
-			beginAtZero: true,
-		  },
-		},
-	  },
-	});
-  }
-  
+}
+
+function displayOutageGraph(graphData) {
+  const graphCanvas = document.getElementById('outageGraph');
+  graphCanvas.innerHTML = '<h2>Outage Graph</h2>';
+
+  new Chart(graphCanvas, {
+    type: 'line',
+    data: graphData,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
